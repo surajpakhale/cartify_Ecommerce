b64570260a2bd5de8d7f120750f267b7a9d09bef
unknown <pakhalesuraj24@gmail.com>
2026-06-30 22:38:31 +0530

    fix: use multer memoryStorage for vercel

--- backend/src/router/product.routes.js
+++ backend/src/router/product.routes.js
@@ -1,16 +1,37 @@
+// const express = require("express");
+// const authMiddleware = require("../middleware/auth.middleware")
+// const authProduct = require("../controller/product.controller")
+// const router = express.Router();
+// const multer = require("multer");
+// const upload =multer({dest : 'uploads/'});
+
+// router.get("/",authProduct.getProduct)
+// router.post("/", authMiddleware.authAdmin, upload.single('image'), authProduct.createProduct)
+// router.get("/:id",authProduct.getProductById)   
+// router.put("/:id", authMiddleware.authAdmin, upload.single('image'), authProduct.updateProduct)
+// router.delete("/:id",authMiddleware.authAdmin,authProduct.deleteProduct)
+
+
+
+// module.exports = router;
+
+
+
 const express = require("express");
 const authMiddleware = require("../middleware/auth.middleware")
 const authProduct = require("../controller/product.controller")
 const router = express.Router();
 const multer = require("multer");
-const upload =multer({dest : 'uploads/'});
+
+// Purana: const upload = multer({dest : 'uploads/'}); ❌
+// Naya Vercel ke liye: ✅
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 router.get("/",authProduct.getProduct)
 router.post("/", authMiddleware.authAdmin, upload.single('image'), authProduct.createProduct)
 router.get("/:id",authProduct.getProductById)   
 router.put("/:id", authMiddleware.authAdmin, upload.single('image'), authProduct.updateProduct)
 router.delete("/:id",authMiddleware.authAdmin,authProduct.deleteProduct)
-
-
 
 module.exports = router;
