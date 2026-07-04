902463a0a615a83c17275100f55e68ffc3ab9c64
unknown <pakhalesuraj24@gmail.com>
2026-07-04 16:37:05 +0530

    cnnffff

--- frontend/cart/src/pages/About.jsx
+++ frontend/cart/src/pages/About.jsx
@@ -27,7 +27,7 @@ const About = () => {
   return (
     <div style={containerStyle}>
       <img
-        src="/suraj_image.jpg"
+        src="/suraj_image .jpg"
         alt="@surajpakhale"
         style={{ width: '180px', height: '180px', borderRadius: '50%', objectFit: 'cover', border: '4px solid #f97316', marginBottom: '20px', boxShadow: '0 4px 20px rgba(249, 115, 22, 0.4)' }}
       />
